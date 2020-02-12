94c004e6faf0a6b01b649b63fbaedc95d6ef85a0
liu-austin <austinliu279@gmail.com>
2020-02-11 20:37:14 -0800

    edits readme

--- README.md
+++ README.md
@@ -1,6 +1,50 @@
 # Recommended Products & Similar Products Microservices for IKEA Backend Project
 
-## Instructions
-Install dependencies - npm install
+Rebuild of backend of IKEA clone recommended products & similar products services deployed to AWS EC2 T2 micros.
 
-Run Server - npm start
+## Table of Contents
+
+1. [Usage](#Usage)
+2. [Requirements](#requirements)
+3. [Development](#development)
+
+
+## Usage
+
+### Setting up database/seeding data
+
+```sh
+npm run setupdb
+```
+
+### start script
+
+```sh
+npm start
+```
+
+## Requirements
+
+An `nvmrc` file is included if using [nvm](https://github.com/creationix/nvm).
+
+- Node 6.13.0
+
+## Development
+
+
+### Installing Dependencies
+
+From within the root directory:
+
+```sh
+npm install -g webpack
+npm install
+```
+
+### CRUD API
+
+Read/GET:
+```
+server.get('/products')
+server.get('/products/:id')
+```
--- server-sdc-pg/index.js
+++ server-sdc-pg/index.js
@@ -75,6 +75,23 @@ if (cluster.isMaster) {
         });
     });
 
+    app.get(`/products/:id`, (req, res) => {
+        return client.get(`products_${req.params._id}`, (err, results) => {
+            if (results) {
+                const resultJSON = JSON.parse(results);
+                return res.set({'source': 'redis'}).status(200).send(resultJSON);
+            } else {
+                return dbPromise.any(`select * from products where id = ${req.params._id}`)
+                .then(result => {
+                    client.setex(`products`, 3600, JSON.stringify(result));
+                    return res.set({'source': 'postgres'}).status(200).send(result);
+                })
+                .catch(err => {
+                    return res.status(404).send(err);
+            });
+            }
+        });
+    });
 
     app.get(`/products`, (req, res) => {
         return client.get(`products`, (err, results) => {
